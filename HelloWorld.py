394380c352a48e461b1f79bb2609d44162de7b5f
Jacy Yang <jacyyang04@gmail.com>
2019-03-30 16:22:00 -0500

    Second commit

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,15 @@
+#Hello World Python#############################################################
+#This is a toy program for an introduction to python.
+
+#recieves string variable and prints into function
+def sup(name):
+    print "Hello " + name + "!"
+    another_function()
+
+#prints 4*5
+def another_function():
+    print 4*5
+
+################################################################################
 print "Hello, World"
+sup("Joel")
